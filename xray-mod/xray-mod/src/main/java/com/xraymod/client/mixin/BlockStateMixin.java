8311766556beb8281e088c146b423cdbf0ab7c8e
Neolixy <yacinenakama@gmail.com>
2026-03-29 22:32:30 +0200

    Update BlockStateMixin.java

--- xray-mod/xray-mod/src/main/java/com/xraymod/client/mixin/BlockStateMixin.java
+++ xray-mod/xray-mod/src/main/java/com/xraymod/client/mixin/BlockStateMixin.java
@@ -5,6 +5,8 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,45 +18,28 @@ import java.util.Set;
 public class BlockStateMixin {
 
     private static final Set<String> VISIBLE_BLOCKS = Set.of(
-            "minecraft:diamond_ore",
-            "minecraft:deepslate_diamond_ore",
-            "minecraft:iron_ore",
-            "minecraft:deepslate_iron_ore",
-            "minecraft:gold_ore",
-            "minecraft:deepslate_gold_ore",
-            "minecraft:emerald_ore",
-            "minecraft:deepslate_emerald_ore",
-            "minecraft:lapis_ore",
-            "minecraft:deepslate_lapis_ore",
-            "minecraft:redstone_ore",
-            "minecraft:deepslate_redstone_ore",
-            "minecraft:coal_ore",
-            "minecraft:deepslate_coal_ore",
-            "minecraft:copper_ore",
-            "minecraft:deepslate_copper_ore",
-            "minecraft:ancient_debris",
-            "minecraft:nether_gold_ore",
-            "minecraft:nether_quartz_ore",
-            "minecraft:chest",
-            "minecraft:trapped_chest",
-            "minecraft:ender_chest",
-            "minecraft:spawner",
-            "minecraft:air",
-            "minecraft:cave_air",
-            "minecraft:void_air"
+            "minecraft:diamond_ore", "minecraft:deepslate_diamond_ore",
+            "minecraft:iron_ore", "minecraft:deepslate_iron_ore",
+            "minecraft:gold_ore", "minecraft:deepslate_gold_ore",
+            "minecraft:emerald_ore", "minecraft:deepslate_emerald_ore",
+            "minecraft:lapis_ore", "minecraft:deepslate_lapis_ore",
+            "minecraft:redstone_ore", "minecraft:deepslate_redstone_ore",
+            "minecraft:coal_ore", "minecraft:deepslate_coal_ore",
+            "minecraft:copper_ore", "minecraft:deepslate_copper_ore",
+            "minecraft:ancient_debris", "minecraft:nether_gold_ore",
+            "minecraft:nether_quartz_ore", "minecraft:chest",
+            "minecraft:trapped_chest", "minecraft:ender_chest",
+            "minecraft:spawner", "minecraft:air",
+            "minecraft:cave_air", "minecraft:void_air"
     );
 
-    @Inject(method = "isFullCube", at = @At("HEAD"), cancellable = true)
-    private void onIsFullCube(CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "getAmbientOcclusionLightLevel", at = @At("HEAD"), cancellable = true)
+    private void onGetAmbientOcclusionLightLevel(BlockView world, BlockPos pos, CallbackInfoReturnable<Float> cir) {
         if (!XRayMod.xrayEnabled) return;
-
         BlockState self = (BlockState) (Object) this;
-        Block block = self.getBlock();
-        Identifier id = Registries.BLOCK.getId(block);
-        String blockId = id.toString();
-
+        String blockId = Registries.BLOCK.getId(self.getBlock()).toString();
         if (!VISIBLE_BLOCKS.contains(blockId)) {
-            cir.setReturnValue(false);
+            cir.setReturnValue(1.0f);
         }
     }
 }
